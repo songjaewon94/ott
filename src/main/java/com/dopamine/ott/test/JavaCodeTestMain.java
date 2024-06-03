59ac16d5f46a2ec90a40d922dd422a6f70f2cb19
sjw <sjg748@naver.com>
2024-06-03 23:35:41 +0900

    1. java main 기반 webclient 테스트 코드

---
+++ src/main/java/com/dopamine/ott/test/JavaCodeTestMain.java
@@ -0,0 +1,26 @@
+package com.dopamine.ott.test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class JavaCodeTestMain {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    public static String webClientCall() {
+        WebClient webClient = WebClient.builder()
+                .baseUrl("http://kobis.or.kr/kobisopenapi/webservice/rest/movie")
+                .build();
+
+   String result = webClient.get()
+                .uri("/searchMovieList.json?key=f5eef3421c602c6cb7ea224104795888")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        System.out.println("result : 한글괴물");
+        return result;
+    }
+
+    public static void main(String[] args) {
+        webClientCall();
+    }
+
+}
